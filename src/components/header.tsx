07f80de689ff6739d36722b9e9a6b624e6b2f4d7
killkill <killkill@killkill.local>
2024-12-15 11:59:07 +0800

    分组路由

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -12,11 +12,11 @@ const linkData = [
 
 export default function Header(){
     const pathname = usePathname();
-    console.log(pathname);
-    const isNotFound = linkData.some(item=>item.path === pathname) || pathname === "/";
-    if (!isNotFound){
-        return null;
-    }
+    // console.log(pathname);
+    // const isNotFound = linkData.some(item=>item.path === pathname) || pathname === "/";
+    // if (!isNotFound){
+    //     return null;
+    // }
     return (
         <div className="absolute w-full z-10">
             <div className="flex justify-between container mx-auto text-white p-8 items-center">
