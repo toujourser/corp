548e7303f44e8b2494c8c68d4f8f041cf5cb0642
killkill <killkill@killkill.local>
2024-12-15 12:13:59 +0800

    finshed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,14 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Comforter } from "next/font/google";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Comforter(
+    {
+        subsets: ['latin'],
+        weight: '400',
+    }
+)
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,11 +22,8 @@ export default function RootLayout({
 }>) {
   return (
       <html lang="en">
-      <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-
-      {children}
+      <body className={ inter.className }>
+          {children}
       </body>
       </html>
   );
